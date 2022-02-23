6ddbfa18cf396a78b8e50889da488924e2d8793a
ejsband <ivb.partner@yandex.ru>
2022-02-24 02:59:19 +0800

    header and footer added

---
+++ src/main/resources/custom/js/common.js
@@ -0,0 +1,3 @@
+// ---------------------------------------------------------------------------------------------------------------------  Copyright
+
+document.querySelector('.footer-wrapper-copyright').innerHTML = '&#169; ' + new Date().getFullYear() + ' - Все права защищены';
