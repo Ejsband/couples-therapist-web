b607251e88ea37e49dc190dc30176640f47f6b93
ejsband <ivb.partner@yandex.ru>
2022-02-23 23:55:23 +0800

    first commit

---
+++ src/main/java/ru/couplestherapistweb/CouplestherapistwebApplication.java
@@ -0,0 +1,13 @@
+package ru.couplestherapistweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouplestherapistwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouplestherapistwebApplication.class, args);
+	}
+
+}
