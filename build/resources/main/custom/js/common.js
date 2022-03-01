8f66b0055c658857c42d733d844d022afbafecfb
ejsband <ivb.partner@yandex.ru>
2022-03-02 03:12:12 +0800

    header and footer completed

--- build/resources/main/custom/js/common.js
+++
@@ -1,3 +0,0 @@
-// ---------------------------------------------------------------------------------------------------------------------  Copyright
-
-document.querySelector('.footer-wrapper-copyright').innerHTML = '&#169; ' + new Date().getFullYear() + ' - Все права защищены';
