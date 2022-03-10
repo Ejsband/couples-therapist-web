a90ec8b018e65a046a1016774658797b766ae562
ejsband <ivb.partner@yandex.ru>
2022-03-11 04:35:26 +0800

    js added to form

--- src/main/resources/custom/js/common.js
+++ src/main/resources/custom/js/common.js
@@ -15,3 +15,28 @@ function hideElement(className, cssAnimationProperty, cssDisplayProperty, timeou
     name.style.animation = cssAnimationProperty;
     setTimeout(() => { name.style.display = cssDisplayProperty;}, timeoutInMillis);
 }
+
+// ---------------------------------------------------------------------------------------------------------------------  Restrict Input Symbols Amount
+
+function limitText(limitField, limitNum) {
+    if (limitField.value.length > limitNum) {
+        limitField.value = limitField.value.substring(0, limitNum);
+    }
+}
+
+// ---------------------------------------------------------------------------------------------------------------------  Restrict e-+ Symbols in Input
+
+document.querySelector('.popUpThree-form-phone-container-right-input').addEventListener("keypress", function (evt) {
+    if (evt.which !== 8 && evt.which !== 0 && evt.which < 48 || evt.which > 57)
+    {
+        evt.preventDefault();
+    }
+});
+
+document.querySelector('.popUpThree-form-email-container-right-input').addEventListener("keypress", function (email) {
+    return String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+});
