e96975e3daea53104012457b34b095f4e8a54941
ejsband <ivb.partner@yandex.ru>
2022-04-14 22:49:50 +0800

    deprecated elements deleted

--- src/main/resources/custom/js/common.js
+++ src/main/resources/custom/js/common.js
@@ -23,18 +23,23 @@ function clearFormFields() {
     document.getElementById('formPhone').value = '';
     document.getElementById('formEmail').value = '';
     document.getElementById('formText').value = '';
-    document.querySelector('.popUpThree-form-name-container-left').style.color = 'black';
-    document.querySelector('.popUpThree-form-phone-container-left').style.color = 'black';
-    document.querySelector('.popUpThree-form-email-container-left').style.color = 'black';
+    document.querySelector('.popUpThree-form-name-container-left').style.color = 'var(--textColorOne)';
+    document.querySelector('.popUpThree-form-phone-container-left').style.color = 'var(--textColorOne)';
+    document.querySelector('.popUpThree-form-email-container-left').style.color = 'var(--textColorOne)';
 }
 
 // ---------------------------------------------------------------------------------------------------------------------  Restrict Input Symbols Amount
 
-function limitText(limitField, limitNum) {
-    if (limitField.value.length > limitNum) {
-        limitField.value = limitField.value.substring(0, limitNum);
+function limitText(limitField, limitNumber) {
+    if (limitField.value.length > limitNumber) {
+        limitField.value = limitField.value.substring(0, limitNumber);
     }
 }
+
+
+document.getElementById('formName').addEventListener('check', () => {
+    return /[A-Za-zА-Яа-яЁё]/i
+})
 
 // ---------------------------------------------------------------------------------------------------------------------  Restrict e-+ Symbols in Phone Input
 
@@ -64,37 +69,37 @@ function sendForm() {
     // let textFlag = false;
 
     if (nameInput.value === '') {
-        nameText.style.color = 'red';
+        nameText.style.color = 'var(--textColorThree)';
     } else {
         nameText.style.color = 'var(--textColorOne)';
         nameFlag = true;
     }
 
     if (phoneInput.value === '' || phoneInput.value.length < 7) {
-        phoneText.style.color = 'red';
+        phoneText.style.color = 'var(--textColorThree)';
     } else {
         phoneText.style.color = 'var(--textColorOne)';
         phoneFlag = true;
     }
 
     if (emailInput.value === '' || !emailInput.value.toLowerCase().match(
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})|(([a-zA-Z\-\d]+\.)+[a-zA-Z]{2,}))$/
         )) {
-        emailText.style.color = 'red';
+        emailText.style.color = 'var(--textColorThree)';
     } else {
         emailText.style.color = 'var(--textColorOne)';
         emailFlag = true;
     }
 
     // if (textarea.value === '') {
-    //     textareaContainer.style.border = 'solid 1px red';
+    //     textareaContainer.style.border = 'solid 1px var(--textColorThree)';
     // } else {
     //     textareaContainer.style.border = 'solid 1px var(--textColorOne)';
     //     textFlag = true;
     // }
 
     if (nameFlag === true && phoneFlag === true && emailFlag === true) {
-        let response = fetch('/appeal', {method: 'POST', body: new FormData(document.querySelector('.popUpThree-form-form'))});
+        let request = fetch('/appeal', {method: 'POST', body: new FormData(document.querySelector('.popUpThree-form-form'))});
         hideElement('.popUpThree', 'closePopUp 0.5s ease', 'none', 380);
         clearFormFields();
     }
